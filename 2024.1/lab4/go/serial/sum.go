f6cdb59c230f24d7233124669b3dcc41545f07d6
Ismael Neto <ismaelneto49@gmail.com>
2024-09-10 15:37:04 -0300

    chore: chunk serial initial implementation

--- 2024.1/lab4/go/serial/sum.go
+++ 2024.1/lab4/go/serial/sum.go
@@ -17,17 +17,29 @@ func readFile(filePath string) ([]byte, error) {
 }
 
 // sum all bytes of a file
-func sum(filePath string) (int, error) {
+func sum(filePath string, chunkSim map[int][]string) (int, error) {
 	data, err := readFile(filePath)
 	if err != nil {
 		return 0, err
 	}
 
 	_sum := 0
-	for _, b := range data {
-		_sum += int(b)
-	}
+	// for _, b := range data {
+	// 	intb := int(b)
+	// 	_sum += intb
+	// 	chunkSim[intb] = append(chunkSim[intb], filePath)
+	// }
 
+
+	for i := 0; i < len(data); i += 100 {
+		chunk := 0
+		for j := 0; j < 100; j++ {
+			intb := int(data[j])
+			_sum += intb
+			chunk += intb
+		}
+		chunkSim[chunk] = append(chunkSim[chunk], filePath)
+	}
 	return _sum, nil
 }
 
@@ -40,8 +52,9 @@ func main() {
 
 	var totalSum int64
 	sums := make(map[int][]string)
+	chunkSim := make(map[int][]string)
 	for _, path := range os.Args[1:] {
-		_sum, err := sum(path)
+		_sum, err := sum(path, chunkSim)
 
 		if err != nil {
 			continue
@@ -59,4 +72,10 @@ func main() {
 			fmt.Printf("Sum %d: %v\n", sum, files)
 		}
 	}
+
+	// for chunk, files := range chunkSim {
+	// 	if len(files) > 1 {
+	// 		fmt.Printf("ALO %d: %v\n", chunk, files)
+	// 	}
+	// }
 }
